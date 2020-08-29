f5a987e5f378d0d7d918c1cff747d49fbe2ca431
moricho <ikeda.morito@gmail.com>
2020-08-29 11:24:49 +0000

    http client: tuning Transport parameter

--- webapp/go/api.go
+++ webapp/go/api.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"time"
 	// "net"
 	// "time"
 )
@@ -52,24 +54,24 @@ type APIShipmentStatusReq struct {
 	ReserveID string `json:"reserve_id"`
 }
 
-// var client = &http.Client{
-// 	Transport: &http.Transport{
-// 			DialContext: (&net.Dialer{
-// 					Timeout:   30 * time.Second,
-// 					KeepAlive: 30 * time.Second,
-// 					DualStack: true,
-// 			}).DialContext,
-// 			MaxIdleConns:          1000,
-// 			MaxIdleConnsPerHost:   500,
-// 			IdleConnTimeout:       90 * time.Second,
-// 			TLSHandshakeTimeout:   10 * time.Second,
-// 			ResponseHeaderTimeout: 10 * time.Second,
-// 			ExpectContinueTimeout: 1 * time.Second,
-// 	},
-// 	Timeout: 60 * time.Second,
-// }
+var client = &http.Client{
+	Transport: &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          1000,
+		MaxIdleConnsPerHost:   500,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	},
+	Timeout: 60 * time.Second,
+}
 
-var client = http.DefaultClient
+// var client = http.DefaultClient
 
 func APIPaymentToken(paymentURL string, param *APIPaymentServiceTokenReq) (*APIPaymentServiceTokenRes, error) {
 	b, _ := json.Marshal(param)
